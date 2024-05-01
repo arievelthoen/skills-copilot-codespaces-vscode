9e7d068d4003b462a031b017d3f7e26c0a8cbc78
arievelthoen <168630983+arievelthoen@users.noreply.github.com>
2024-05-01 12:17:22 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,10 @@
+// Create web server
+// Start web server
+// Load necessary modules
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+var comments = [];
+var port = 3000;
+// Create server
